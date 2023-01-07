fa3f1f1d9a355a11a59046a236656858fc4d4a7b
Milesfeng <38985858+Milesfeng@users.noreply.github.com>
2023-01-08 03:54:06 +0800

    update: add apt tools

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e
 
 sudo apt update
-
-sudo apt-get install net-tools nmap -y
+sudo apt-get install nano \
+                     wget \ 
+                     git \
+                     net-tools \
+                     nmap \
+                     tree \
+                     ssh \
+                     vim \
+                     wget \
+                     curl -y
